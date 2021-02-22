1c33065cff05ecf988ba03c8ecf4e11b79ddf098
beardbytes <adi.maheshwari53@gmail.com>
2021-02-22 14:51:21 +0530

    Added dummy crendials

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://dbUser:mongodbatlas@cluster0.3vudb.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://dbUser:<password>@cluster0.3vudb.mongodb.net/local?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
